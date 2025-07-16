49d451e2e22bf2e1a295325177e5f14c6bb9776f
DHRUBA SAIKIA <dhrubasaikia937@gmail.com>
2025-07-16 11:04:24 +0530

    init comment

---
+++ src/VariableExample.java
@@ -0,0 +1,16 @@
+public class VariableExample
+{
+    public static void main(String args[])
+    {
+        int age = 25;
+        double height = 5.9;
+        char grade = 'A';
+        String name = "Dhruba";
+        boolean isStudent = true;
+        System.out.println("name : "+name);
+        System.out.println("Age : "+age+"Years");
+        System.out.println("Height : "+height+"feet");
+        System.out.println("Grade : "+grade);
+        System.out.println("Is a Student : "+isStudent);
+    }
+}
